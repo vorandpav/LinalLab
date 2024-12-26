1395f2f15e95d3b5d421df69b18433156344a870
Andrew Vorobev <vorandpav@yandex.ru>
2024-12-27 00:19:11 +0300

    some_changes

--- matrix.py
+++ matrix.py
@@ -1,7 +1,9 @@
 class Matrix:
+    accuracy: int = 2
     num_rows: int
     num_columns: int
     square: bool
+
     tracer: float
     determinant: float
 
@@ -10,10 +12,11 @@ class Matrix:
     column_indices: list[int]
 
     def __init__(self, input_string: str):
-        input_string = [row.split() for row in input_string.split('\n')]
+        input_string = input_string.split('\n')
 
-        self.num_rows = int(input_string[0][0])
-        self.num_columns = int(input_string[0][1])
+        size_string = input_string[0].split()
+        self.num_rows = int(size_string[0])
+        self.num_columns = int(size_string[1])
         self.square = self.num_rows == self.num_columns
 
         self.values = []
@@ -24,34 +27,41 @@ class Matrix:
             for row in range(self.num_rows):
                 self.row_sizes.append(0)
         else:
-            matrix_list: list[list[float]] = [[float(value) for value in row] for row in input_string[1:]]
+            self.accuracy = int(input_string[1])
 
             for row in range(self.num_rows):
+                row_string = input_string[row + 2].split()
                 self.row_sizes.append(self.row_sizes[-1])
 
                 for column in range(self.num_columns):
-                    if matrix_list[row][column] != 0:
-                        self.values.append(matrix_list[row][column])
+                    element = round(float(row_string[column]), self.accuracy)
+                    if element != 0:
+                        self.values.append(element)
                         self.column_indices.append(column)
                         self.row_sizes[-1] += 1
 
     def __getitem__(self, item: [int, int]) -> float:
         row, column = item[0] - 1, item[1] - 1
-        if len(self.values) != 0 and column in self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]:
+        if (len(self.values) != 0
+                and column in self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]):
             return self.values[
-                self.row_sizes[row] +
-                self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]].index(column)
-                ]
+                self.row_sizes[row]
+                + self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]].index(column)]
         else:
             return 0
 
     def __setitem__(self, item: [int, int], value: float) -> None:
         row, column = item[0] - 1, item[1] - 1
+        value = round(value, self.accuracy)
+
         if value == 0:
-            if len(self.values) != 0 and column in self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]:
+            if (len(self.values) != 0
+                    and column in self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]):
                 for index in range(
-                        self.row_sizes[row] + self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]].index(
-                            column), self.row_sizes[-1] - 1):
+                        self.row_sizes[row]
+                        + self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]
+                                .index(column),
+                        self.row_sizes[-1] - 1):
                     self.values[index] = self.values[index + 1]
                     self.column_indices[index] = self.column_indices[index + 1]
 
@@ -70,9 +80,9 @@ class Matrix:
 
             elif column in self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]:
                 self.values[
-                    self.row_sizes[row] +
-                    self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]].index(column)
-                    ] = value
+                    self.row_sizes[row]
+                    + self.column_indices[self.row_sizes[row]:self.row_sizes[row + 1]]
+                    .index(column)] = value
 
             else:
                 self.values.append(self.values[-1])
@@ -88,17 +98,66 @@ class Matrix:
                 self.values[self.row_sizes[row + 1] - 1] = value
 
     def _matrices_sum(self, other: 'Matrix') -> 'Matrix':
-        result_matrix = Matrix(f'{self.num_rows} {self.num_columns}')
+        result_matrix = Matrix("0 0")
+        result_matrix.accuracy = self.accuracy
+        result_matrix.num_rows = self.num_rows
+        result_matrix.num_columns = self.num_columns
+        result_matrix.square = self.square
+
         for row in range(1, self.num_rows + 1):
-            for column in range(1, self.num_columns + 1):
-                result_matrix[row, column] = self[row, column] + other[row, column]
+            result_matrix.row_sizes.append(result_matrix.row_sizes[-1])
+
+            first_ptr = self.row_sizes[row - 1]
+            second_ptr = other.row_sizes[row - 1]
+
+            while first_ptr < self.row_sizes[row] or second_ptr < other.row_sizes[row]:
+                if (first_ptr < self.row_sizes[row]
+                        and second_ptr < other.row_sizes[row]
+                        and self.column_indices[first_ptr] == other.column_indices[second_ptr]):
+                    if round(self.values[first_ptr]
+                             + other.values[second_ptr], result_matrix.accuracy) != 0:
+                        result_matrix.values.append(
+                            round(self.values[first_ptr]
+                                  + other.values[second_ptr], result_matrix.accuracy))
+                        result_matrix.column_indices.append(self.column_indices[first_ptr])
+                        result_matrix.row_sizes[-1] += 1
+
+                    first_ptr += 1
+                    second_ptr += 1
+
+                elif (first_ptr < self.row_sizes[row]
+                      and (second_ptr >= other.row_sizes[row]
+                           or self.column_indices[first_ptr] < other.column_indices[second_ptr])):
+                    result_matrix.values.append(self.values[first_ptr])
+                    result_matrix.column_indices.append(self.column_indices[first_ptr])
+                    result_matrix.row_sizes[-1] += 1
+                    first_ptr += 1
+
+                else:
+                    if round(other.values[second_ptr], result_matrix.accuracy) != 0:
+                        result_matrix.values.append(round(other.values[second_ptr], result_matrix.accuracy))
+                        result_matrix.column_indices.append(other.column_indices[second_ptr])
+                        result_matrix.row_sizes[-1] += 1
+                    second_ptr += 1
+
         return result_matrix
 
     def _matrix_and_number_sum(self, other: float) -> 'Matrix':
-        result_matrix = Matrix(f'{self.num_rows} {self.num_columns}')
+        result_matrix = Matrix("0 0")
+        result_matrix.accuracy = self.accuracy
+        result_matrix.num_rows = self.num_rows
+        result_matrix.num_columns = self.num_columns
+        result_matrix.square = self.square
+
         for row in range(1, self.num_rows + 1):
-            for column in range(1, self.num_columns + 1):
-                result_matrix[row, column] = self[row, column] + other
+            result_matrix.row_sizes.append(result_matrix.row_sizes[-1])
+            for element in range(self.row_sizes[row - 1], self.row_sizes[row]):
+                if round(self.values[element] + other, result_matrix.accuracy) != 0:
+                    result_matrix.values.append(
+                        round(self.values[element] + other, result_matrix.accuracy))
+                    result_matrix.column_indices.append(self.column_indices[element])
+                    result_matrix.row_sizes[-1] += 1
+
         return result_matrix
 
     def __add__(self, other) -> 'Matrix':
@@ -108,25 +167,68 @@ class Matrix:
             return self._matrices_sum(other)
 
         else:
-
             return self._matrix_and_number_sum(other)
 
     def __radd__(self, other):
         return self + other
 
-    def _matrices_mul(self, other: 'Matrix') -> 'Matrix':
-        result_matrix = Matrix(f'{self.num_rows} {other.num_columns}')
+    def __iadd__(self, other):
+        return self + other
+
+    def transpose(self) -> 'Matrix':
+        column_row = [[] for _ in range(self.num_columns)]
+        column_value = [[] for _ in range(self.num_columns)]
+
         for row in range(1, self.num_rows + 1):
-            for column in range(1, other.num_columns + 1):
-                for i in range(1, self.num_columns + 1):
-                    result_matrix[row, column] += self[row, i] * other[i, column]
+            for element in range(self.row_sizes[row - 1], self.row_sizes[row]):
+                column_row[self.column_indices[element]].append(row - 1)
+                column_value[self.column_indices[element]].append(self.values[element])
+
+        result_matrix = Matrix("0 0")
+        result_matrix.accuracy = self.accuracy
+        result_matrix.num_rows = self.num_columns
+        result_matrix.num_columns = self.num_rows
+        result_matrix.square = self.square
+
+        for column in range(self.num_columns):
+            result_matrix.row_sizes.append(result_matrix.row_sizes[-1])
+            for element in range(len(column_row[column])):
+                result_matrix.values.append(column_value[column][element])
+                result_matrix.column_indices.append(column_row[column][element])
+                result_matrix.row_sizes[-1] += 1
+
         return result_matrix
 
+    def _matrices_mul(self, other: 'Matrix') -> 'Matrix':
+        # other = other.transpose()
+        #
+        # result_matrix = Matrix("0 0")
+        # result_matrix.accuracy = self.accuracy
+        # result_matrix.num_rows = self.num_rows
+        # result_matrix.num_columns = other.num_rows
+        # result_matrix.square = result_matrix.num_rows == result_matrix.num_columns
+        #
+        # for row in range(1, self.num_rows + 1):
+        #     result_matrix.row_sizes.append(result_matrix.row_sizes[-1])
+        #     for other_row
+        pass
+
     def _matrix_and_number_mul(self, other: float) -> 'Matrix':
-        result_matrix = Matrix(f'{self.num_rows} {self.num_columns}')
+        result_matrix = Matrix("0 0")
+        result_matrix.accuracy = self.accuracy
+        result_matrix.num_rows = self.num_rows
+        result_matrix.num_columns = self.num_columns
+        result_matrix.square = self.square
+
         for row in range(1, self.num_rows + 1):
-            for column in range(1, self.num_columns + 1):
-                result_matrix[row, column] = self[row, column] * other
+            result_matrix.row_sizes.append(result_matrix.row_sizes[-1])
+            for element in range(self.row_sizes[row - 1], self.row_sizes[row]):
+                if round(self.values[element] * other, result_matrix.accuracy) != 0:
+                    result_matrix.values.append(
+                        round(self.values[element] * other, result_matrix.accuracy))
+                    result_matrix.column_indices.append(self.column_indices[element])
+                    result_matrix.row_sizes[-1] += 1
+
         return result_matrix
 
     def __mul__(self, other):
@@ -141,8 +243,13 @@ class Matrix:
     def __rmul__(self, other):
         return self * other
 
+    def __imul__(self, other):
+        return self * other
+
     def get_list(self) -> list[list[float]]:
-        return [[self[row + 1, column + 1] for column in range(self.num_columns)] for row in range(self.num_rows)]
+        return [[self[row + 1, column + 1]
+                 for column in range(self.num_columns)]
+                for row in range(self.num_rows)]
 
     def _calculate_tracer(self) -> None:
         self.tracer = 0
@@ -211,7 +318,11 @@ class Matrix:
 
 
 if __name__ == '__main__':
-    m = Matrix("3 3\n-4 -1 2\n10 4 -1\n8 3 1")
+    m = Matrix("3 3\n"
+               "2\n"
+               "-4 -1 2\n"
+               "10 4 -1\n"
+               "8 3 1")
     print(m.get_list())
     print("just print\n")
 
@@ -219,7 +330,11 @@ if __name__ == '__main__':
     print(m.get_list())
     print("+number\n")
 
-    m = m + Matrix("3 3\n-1 -1 -1\n-1 -1 -1\n-1 -1 -1")
+    m = m + Matrix("3 3\n"
+                   "2\n"
+                   "-1 0 0\n"
+                   "0 -5.01 -1\n"
+                   "0 0 -2")
     print(m.get_list())
     print("+matrix\n")
 
@@ -227,7 +342,11 @@ if __name__ == '__main__':
     print(m.get_list())
     print("*number\n")
 
-    m = m * Matrix("3 3\n0.5 0 0\n0 0.5 0\n0 0 0.5")
+    m = m * Matrix("3 3\n"
+                   "2\n"
+                   "0.5 0 0\n"
+                   "0 0.5 0\n"
+                   "0 0 0.5")
     print(m.get_list())
     print("*matrix\n")
 
@@ -236,3 +355,23 @@ if __name__ == '__main__':
 
     m.print_determinant_and_invertibility()
     print("determinant and invertibility\n")
+
+    m = Matrix("6 6\n"
+               "2\n"
+               "1 0 0 0 2 0\n"
+               "0 0 3 4 0 0\n"
+               "0 0 0 0 0 0\n"
+               "0 0 0 8 0 5\n"
+               "0 0 0 0 0 0\n"
+               "0 7 1 0 0 6")
+    print(m.get_list())
+    print("just print\n")
+
+    print(m.values)
+    print("values\n")
+
+    print(m.row_sizes)
+    print("row_sizes\n")
+
+    print(m.column_indices)
+    print("column_indices\n")
